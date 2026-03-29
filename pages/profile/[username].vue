ffc17d3242bc0745f63b9856b18fd1e1d79578bd
MrMarzeero <otaviorp2014@gmail.com>
2026-03-28 21:23:52 -0300

    fuck

--- pages/profile/[username].vue
+++ pages/profile/[username].vue
@@ -1,27 +1,20 @@
 <script setup lang="ts">
-import { computed } from 'vue'
-import { useRoute } from '#imports'
-import { useAxios } from '~/composables/useAxios'
-
-const route = useRoute()
-const username = computed(() => route.params.username as string)
-
 const { data: user, pending, error } = await useAsyncData(
   () => `user-${username.value}`,
   async () => {
     const { get } = useAxios()
-    const response = await get(`/users/username/${encodeURIComponent(username.value)}`)
-    return response.data
-  },
-  { watch: [username] }
+    const res = await get(`/api/v1/users/username/${username.value}`)
+    console.log('API RESPONSE:', res.data)
+    return res.data
+  }
 )
-
-const userId = computed(() => user.value?.id)
 </script>
 
 <template>
   <div v-if="pending">Carregando...</div>
-  <div v-else-if="error">Usuário não encontrado</div>
+  <div v-else-if="error">
+    {{ error }}
+  </div>
   <div v-else-if="user">
     {{ user.full_name }}
   </div>
